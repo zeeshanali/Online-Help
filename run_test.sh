35025db1f80dbf9e31781b21441a83fdc19371c9
Joe Grund <grundjoseph@gmail.com>
2017-04-26 11:54:23 -0400

    update travis file
    Signed-off-by: Joe Grund <grundjoseph@gmail.com>

--- run_test.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-# There are no tests to run, but CI needs a results file.
-echo '<testsuites><testsuite name="help-docs" package="help-docs" timestamp="2014-07-31" id="0" hostname="localhost" tests="0" errors="0" failures="0" skipped="0" time="0.008"></testsuite></testsuites>' > help-docs-results.xml
